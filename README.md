02b9e0b88ff9cacc329964864a34be1defbb8448
Michael Mroz <michaelmroz01111@gmail.com>
2024-04-29 16:39:26 -0300

    added buttons to start screen to transition to main view

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Personal project game where the goal is to make the phrase "Hello World" appear 
 Currently the game is being made using only HTML/CSS/Vanilla JS, but my plan is to refactor the project using MERN stack once the basic functionality of the game is playable. 
 
 Current MERN ideas:
-MongoDB: For storing World objects containing difficulty/name/game attributes. 
+MongoDB: For storing World objects containing difficulty/name/hint/play() attributes. 
 Express & Node: API calls to fetch World objects from DB to use in games. 
 React: For frontend. 
--- scripts.js
+++ scripts.js
@@ -12,6 +12,47 @@ mainLayout.map(component => component.style.visibility = "hidden");
 const startScreen = document.createElement("section")
 startScreen.id = "start_screen";
 document.getElementsByTagName("main")[0].insertBefore(startScreen, menu);
-startScreen.textContent = "Welcome!\n\n\n" 
-startScreen.textContent += "Clear the Worlds that appear before you by making the phrase 'Hello World' appear on the screen.\n\n" 
-startScreen.textContent += "Sounds simple, right? Select a starting difficulty:";
+const introMsg = document.createElement("p");
+startScreen.appendChild(introMsg);
+introMsg.textContent = "Welcome!\n\n\n" 
+introMsg.textContent += "Clear the Worlds that appear before you by making the phrase 'Hello World' appear on the screen.\n\n" 
+introMsg.textContent += "Sounds simple... right? Select a starting difficulty:";
+const easyButton = document.createElement("button");
+const mediumButton = document.createElement("button");
+const hardButton = document.createElement("button");
+const startButtons = [easyButton, mediumButton, hardButton];
+startButtons.map(button => startScreen.appendChild(button));
+const difficulties = ["Easy", "Medium", "Hard"];
+
+//click handler for start buttons, switch to main view then fetch a world to start
+const startGame = (difficulty) => {
+    console.log("This will call a", difficulty, "world from the server!");
+    //not yet implemented: currentWorld = fetchWorld(difficulty);
+    startScreen.style.display = "none";
+    mainLayout.map(component => component.style.visibility = "visible");
+}
+
+for (let i = 0; i < 3; i++) {
+    startButtons[i].textContent = difficulties[i];
+    startButtons[i].addEventListener('click', () => startGame(difficulties[i]));
+}
+
+/*
+Core gameplay loop:
+
+initialize: 
+worlds_cleared = 0;
+currentWorld = world fetched from starting button
+
+while worlds_cleared < 4: 
+  currentWorld.play(); 
+  
+  ^play function is a while loop that terminates once cleared. 
+  once cleared:
+  worlds_cleared++
+  fetch nextWorld from server
+  currentWorld = nextWorld 
+
+  ---once worlds_cleared hits 4, fetch the final boss world
+  ^could implement the final boss world locally? 
+*/
