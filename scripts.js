38be396d6332c479c8b48c009bde27966483ea73
Michael Mroz <michaelmroz01111@gmail.com>
2024-04-29 22:26:28 -0300

    added placeholder code for gameplay

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ const gameArea = document.getElementById("game_area");
 const levelInfo = document.getElementById("level_info");
 const mainLayout = [menu, outputArea, hintBox, gameArea, levelInfo]
 
+//hide main view until start screen is finished
 mainLayout.map(component => component.style.visibility = "hidden");
 
 const startScreen = document.createElement("section")
@@ -26,7 +27,7 @@ const difficulties = ["Easy", "Medium", "Hard"];
 
 //click handler for start buttons, switch to main view then fetch a world to start
 const startGame = (difficulty) => {
-    console.log("This will call a", difficulty, "world from the server!");
+    console.log("This will call a", difficulty, "world object from the server!");
     //not yet implemented: currentWorld = fetchWorld(difficulty);
     startScreen.style.display = "none";
     mainLayout.map(component => component.style.visibility = "visible");
@@ -36,23 +37,45 @@ for (let i = 0; i < 3; i++) {
     startButtons[i].textContent = difficulties[i];
     startButtons[i].addEventListener('click', () => startGame(difficulties[i]));
 }
+//boolean flag to allow keyboard input
+let allowInput = false;
+let worldsCleared = 0;
+let currentWorld = "this will be replaced by a world object";
 
-/*
-Core gameplay loop:
+while (worldsCleared < 4) {
+  console.log("here's where currentWorld.play() gets called");
+  worldsCleared++;
+  console.log("heres where a new world is fetched and assigned as currentWorld");
+}
 
-initialize: 
-worlds_cleared = 0;
-currentWorld = world fetched from starting button
+console.log("this is where the final world is fetched and .play() gets called")
 
-while worlds_cleared < 4: 
-  currentWorld.play(); 
-  
-  ^play function is a while loop that terminates once cleared. 
-  once cleared:
-  worlds_cleared++
-  fetch nextWorld from server
-  currentWorld = nextWorld 
+//to be refactored into final world object's .play() function
+const finalWorld = () => {
+  let letters = ["H", "e", "l", "l", "o", "W", "o", "r", "l", "d"];
+  let lettersToEarn = [];
 
-  ---once worlds_cleared hits 4, fetch the final boss world
-  ^could implement the final boss world locally? 
-*/
+  for (let i = 0; i < 5; i++) {
+    let randIndex = Math.floor(Math.random() * letters.length);
+    lettersToEarn.push(letters[randIndex]);
+    letters.splice(randIndex, 1);
+  }
+  console.log("Letters are:", letters);
+  console.log("to earn are:", lettersToEarn);
+}
+
+const startingWorld = () => {
+  while (output.textContent !== "Hello World"){
+    
+  }
+  output.textContent = "Cleared!";
+}
+
+document.addEventListener("keydown", (e) => {
+  if (!allowInput) {
+    return;
+  }
+
+})
+
+startingWorld();
